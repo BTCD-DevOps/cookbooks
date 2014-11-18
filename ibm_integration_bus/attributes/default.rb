10f24b082fe8771a2a04eb5d5983074f041802c6
“btcd.devops” <btcd.devops@gmail.com>
2014-11-18 15:45:37 -0600

    Added IIB install parameters to cookbook
    added IIB install parameters to cookbook

--- ibm_integration_bus/attributes/default.rb
+++ ibm_integration_bus/attributes/default.rb
@@ -11,8 +11,8 @@
 #     IBM - initial implementation
 #
 ################################################################################
-default['ibm_integration_bus']['package_site_url'] = nil
-default['ibm_integration_bus']['package_name']     = "9.0.0-IIB-LINUXX64-DEVELOPER.tar.gz"
+default['ibm_integration_bus']['package_site_url'] = “file:///vagrant/package”
+default['ibm_integration_bus']['package_name']     = "integrationbus_developer.amd64_linux_2.dev_all.tar"
 default['ibm_integration_bus']['account_username'] = "iibuser"
 default['ibm_integration_bus']['account_password'] = nil
 default['ibm_integration_bus']['iib_nodes']        = nil
